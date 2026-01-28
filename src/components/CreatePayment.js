b26280e4aa4d585e852c7aca09e0e869845f22ce
hassanmubiru <hassanmubiru7@gmail.com>
2026-01-28 05:08:17 +0300

    modified:   src/components/CreatePayment.js

--- src/components/CreatePayment.js
+++ src/components/CreatePayment.js
@@ -117,6 +117,8 @@ const CreatePayment = ({ onPaymentCreated }) => {
     setPaymentLink('');
     setCreatedPayment(null);
     setCopied(false);
+    setIsSending(false);
+    setTxResult(null);
   };
 
   const copyToClipboard = (text) => {
